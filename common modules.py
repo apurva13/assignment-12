7f2e53304dfb8ce5b42223d70c100650a78cb37a
Apurva Gupta <37153621+apurva13@users.noreply.github.com>
2018-09-01 18:42:47 +0530

    Update common modules.py

--- common modules.py
+++ common modules.py
@@ -14,6 +14,7 @@ webbrowser.open("https://www.youtube.com/watch?v=g7eEYUtvRLc")
 
 
 #Q.3- Write a program to rename all the files in a directory and convert them into .jpg file format.
+import os,sys
 address = r'C:\Users\Apurva Gupta\Desktop\Python\assignment 12'
 for file in os.listdir(address):
     oldfile = os.path.join(address,file)
